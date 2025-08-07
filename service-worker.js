bc882cd3c6d082ff959b71850cb84cd4cd32656f
Trek-Works <github.travel@gmail.com>
2025-08-07 17:32:28 +1000

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activated');
-});
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
